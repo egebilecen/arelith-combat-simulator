83fea981604eee5d060a6ccfbaa3e905e151fcc9
Ege Bilecen <ege@r0m.cc>
2023-12-17 18:56:00 +0000

    added comments

--- src/item/item.rs
+++ src/item/item.rs
@@ -1,5 +1,5 @@
-use crate::dice::Dice;
 use super::Damage;
+use crate::dice::Dice;
 
 pub fn get_keen_increase(threat_range: i32) -> i32 {
     20 - threat_range + 1
@@ -8,10 +8,10 @@ pub fn get_keen_increase(threat_range: i32) -> i32 {
 #[derive(PartialEq)]
 #[allow(unused)]
 pub enum ItemProperty {
-    AttackBonus(i32),
-    EnchantmentBonus(i32),
-    MassiveCrit(Dice),
+    AttackBonus(i32),      // Not implemented, it won't increase the character's AB.
+    EnchantmentBonus(i32), // Not implemented, it won't increase the character's AB but it will increase the damage.
     DamageBonus(Damage),
+    MassiveCrit(Dice),
     Keen,
     ThreatRangeOverride(i32),
     CriticalMultiplierOverride(i32),
