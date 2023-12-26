b1ab0bf19e3afd51c3dfb50fead4c767e967b685
Ege Bilecen <ege@r0m.cc>
2023-12-26 23:25:11 +0000

    implemented From<&str> for SizeCategory

--- src/size.rs
+++ src/size.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, PartialEq, PartialOrd, Serialize, Deserialize)]
 pub enum SizeCategory {
@@ -7,10 +7,24 @@ pub enum SizeCategory {
     Medium,
     Large,
     Huge,
+    Unknown,
 }
 
 impl Default for SizeCategory {
     fn default() -> Self {
         Self::Medium
+    }
+}
+
+impl From<&str> for SizeCategory {
+    fn from(value: &str) -> Self {
+        match value.to_lowercase().as_str() {
+            "tiny" => Self::Tiny,
+            "small" => Self::Small,
+            "medium" => Self::Medium,
+            "large" => Self::Large,
+            "huge" => Self::Huge,
+            _ => Self::Unknown,
+        }
     }
 }
